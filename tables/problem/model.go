3484383bc19d063e7d6067997fef79aaa5eedfd7
U-T-kuroitigo <git.cq.kx@gmail.com>
2024-07-01 15:24:36 +0900

    add/problem
    problemテーブル用のコードを追加

---
+++ tables/problem/model.go
@@ -0,0 +1,29 @@
+package problem
+
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+)
+
+type Problem struct {
+	ProblemID string `json:"problem_id" gorm:"type:varchar(255);primaryKey;not null" validate:"max=32"`
+	SituationID string `json:"situation_id" gorm:"type:varchar(255);not null" validate:"max=32"`
+	ProblemTitle string `json:"problem_title" gorm:"not null"`
+	ProblemText string `json:"problem_text" gorm:"not null"`
+	ProblemExplanation string `json:"problem_explanation" gorm:"not null"`
+	CreatedAt time.Time
+  UpdatedAt time.Time
+  DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
+
+func ValidateProblem(problem *Problem) error {
+	return validate.Struct(problem)
+}
