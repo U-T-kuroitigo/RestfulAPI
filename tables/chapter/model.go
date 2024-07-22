9eb3d3e45e2294a5955ab93ac5f95c86d3a03e19
U-T-kuroitigo <git.cq.kx@gmail.com>
2024-07-22 16:39:42 +0900

    change/tables/theme&chapter
    themeとchapterにimgのパス用のカラムを追加した。

--- tables/chapter/model.go
+++ tables/chapter/model.go
@@ -13,6 +13,7 @@ type Chapter struct {
 	ChapterID      string                           `json:"chapter_id" gorm:"type:varchar(255);primaryKey;not null" validate:"max=32"`
 	ThemeID        string                           `json:"theme_id" gorm:"type:varchar(255);not null" validate:"max=32"`
 	ChapterTitle   string                           `json:"chapter_title" gorm:"type:varchar(255);not null" validate:"max=12"`
+	ChapterImg     string                           `json:"chapter_img" gorm:"not null"`
 	Situation      []situation.Situation            `gorm:"foreignKey:ChapterID;references:ChapterID"`
 	ExtraSituation []extra_situation.ExtraSituation `gorm:"foreignKey:ChapterID;references:ChapterID"`
 	CreatedAt      time.Time
